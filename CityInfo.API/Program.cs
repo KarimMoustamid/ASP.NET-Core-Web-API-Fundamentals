51cfdd2176562d0cb1f31ff2ee0bc00e0425a2a7
karimmoustamid <moustamid.teck@gmail.com>
2024-08-28 14:30:07 -0600

    Enhance controller support with XML formatting
    Updated the controller configuration to include a 406 Not Acceptable response for mismatched Accept headers and added XML Data Contract Serializer formatters to support XML serialization/deserialization. Additionally, commented out the custom ProblemDetails configuration section.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,18 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers(); // Add this line to include controller support
+
+ // Add this line to include controller support
+builder.Services.AddControllers(
+    // This option causes the controller to return a 406 Not Acceptable response when the request's Accept header does not match any of the controller's usable media types.
+    option => option.ReturnHttpNotAcceptable = true
+)
+// Adds the XML Data Contract Serializer formatters to MVC so it can deserialize the XML received in requests and serialize XML in the responses.
+.AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddProblemDetails(option =>
-{
-    option.CustomizeProblemDetails = ctx =>
-    {
-        // Adding a custom property "additionalInfo" to the ProblemDetails response
-        ctx.ProblemDetails.Extensions.Add("Server", Environment.MachineName);
-    };
-});
+// builder.Services.AddProblemDetails(option =>
+// {
+//     option.CustomizeProblemDetails = ctx =>
+//     {
+//         // Adding a custom property "additionalInfo" to the ProblemDetails response
+//         ctx.ProblemDetails.Extensions.Add("Server", Environment.MachineName);
+//     };
+// });
 
 var app = builder.Build();
 
--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0925c0c84e73e4f7ecb2dbd32b7274b9edeb8d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b1a99c1f2d1dc919fcb54d4cce89576fb35fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
